6fb855b29c8882c8bf49a28f76f9b1c1d871d3c9
ollenber <ollenber@OLLENBERGER851.americas.hpqcorp.net>
2009-05-15 16:11:10 -0700

    Made stream pipeline sends ahead of acks.

--- stream.erl
+++ stream.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 -export([test/0]).
--export([start_link/1, make_stream/1, make_sender/2]).
+-export([start_link/2, make_channel/1, make_sender/2, ready_to_receive/1]).
 -export([init/1, terminate/2, handle_info/2, handle_call/3, handle_cast/2, code_change/3]).
 
 run_test(Test) ->
@@ -33,151 +33,157 @@ test_channel_send() ->
 	{ok, Gateway2} = gateway:start_link(),
 	gateway:connect(Gateway2, {127,0,0,1}, 1234, 1),
 	
-	{ok, Stream1} = stream:start_link(Gateway1),
-	{ok, Stream2} = stream:start_link(Gateway2),
+	{ok, Stream1} = stream:start_link(Gateway1, stream),
+	{ok, Stream2} = stream:start_link(Gateway2, stream),
 	
-	{Local, Remote} = stream:make_stream(Stream1),
+	Channel = stream:make_channel(Stream1),
 	
-	Sender = stream:make_sender(Stream2, Remote),
+	Sender = stream:make_sender(Stream2, Channel),
 	
-	Sender ! <<"this is a test">>,
-	Sender ! close,
+	Data = <<"this is a test! this is a test! this is a test! this is a test! this is a test! this is a test! ">>,
+	Sender(Data),
+	Sender(close),
 	
+	io:format("~p~n", [recv_loop([], Channel)]).
+
+recv_loop(Acc, Channel) ->
+	ready_to_receive(Channel),
 	receive
-		{packet, Local, Data} -> io:format("~p~n", [Data])
+		{packet, Channel, Data} ->
+			recv_loop([Data|Acc], Channel);
+		{close, Channel} ->
+			iolist_to_binary(lists:reverse(Acc))
 	end.
 
-start_link(Gateway) ->
-	gen_server:start_link(?MODULE, [Gateway], []).
+start_link(Gateway, ServiceName) ->
+	gen_server:start_link(?MODULE, [Gateway, {?MODULE, ServiceName}], []).
 	
-init([Gateway]) ->
+init([Gateway, ServiceName]) ->
 	Name = gateway:get_name(Gateway),
 	Handler = fun
 		(cast, _, {heartbeat, Stream, Pid}) ->
 			Pid ! {heartbeat, Stream};
 		(cast, From, {packet, Stream, Pid, Sequence, Data, AckPid}) ->
-			Pid ! {packet, From, Stream, Sequence, Data, AckPid}; % to stream_worker()
+			Pid ! {packet, From, Stream, Sequence, Data, AckPid}; % to receiver_loop()
 		(cast, _, {ack, Stream, Pid, Sequence}) ->
-			Pid ! {ack, Stream, Sequence}; % to send()
+			Pid ! {ack, Stream, Sequence}; % to sender_loop()
 		(cast, _, {close, Stream, Pid, Sequence}) ->
-			Pid ! {close, Stream, Sequence} % to stream_worker()
+			Pid ! {close, Stream, Sequence} % to receiver_loop()
 	end,
 	
-	ok = gateway:register(Gateway, ?MODULE, Handler),
-	RPC_Cast = gateway:cast(Gateway, ?MODULE),
+	ok = gateway:register(Gateway, ServiceName, Handler),
+	RPC_Cast = gateway:cast(Gateway, ServiceName),
 	
 	{ok, {RPC_Cast, Name}}.
 	
-stream_worker(RPC_Cast, Stream, Pid, Sequence) ->
-	InactivityTimeout = 300000, % 30 seconds - three times the 10 second heartbeat
+receiver_loop(RPC_Cast, Channel, Sequence, ReceiverPid, Active) ->
+	{channel, _, Stream, _, _} = Channel,
+	InactivityTimeout = 900000, % 90 seconds - three times the 30 second heartbeat
+	
 	receive
 		{heartbeat, Stream} ->
 			% reset inactivity timeout
-			stream_worker(RPC_Cast, Stream, Pid, Sequence);
-		{packet, From, Stream, Sequence, Data, AckPid} ->
-			Pid ! {packet, Stream, Data},
+			receiver_loop(RPC_Cast, Channel, Sequence, ReceiverPid, Active);
+		
+		active ->
+			receiver_loop(RPC_Cast, Channel, Sequence, ReceiverPid, active);
+			
+		{packet, From, Stream, Sequence, Data, AckPid} when Active =:= active ->
+			ReceiverPid ! {packet, Channel, Data},
 			RPC_Cast(From, {ack, Stream, AckPid, Sequence}),
-			stream_worker(RPC_Cast, Stream, Pid, Sequence+1);
+			receiver_loop(RPC_Cast, Channel, Sequence + 1, ReceiverPid, inactive);
 		{packet, _, Stream, S, _, _} when S < Sequence ->
 			drop_duplicate,
-			stream_worker(RPC_Cast, Stream, Pid, Sequence);
-		{close, Stream, Sequence} ->
-					Pid ! {close, Stream},
-			terminate
+			receiver_loop(RPC_Cast, Channel, Sequence, ReceiverPid, Active);
+		
+		{close, Stream, Sequence} when Active =:= active ->
+			ReceiverPid ! {close, Channel},
+			closed
 	after InactivityTimeout ->
-		terminate % just give up. the remote end will give up eventually
+		timeout % just give up. the remote end will give up eventually
 	end.
 
-sender_loop(Server, Channel) ->
+sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, AckSequence) ->
 	receive
-		close ->
-			close(Server, Channel);
-		Data when is_binary(Data) ->
-			case send(Server, Channel, Data) of
-				{ok, NewChannel} -> sender_loop(Server, NewChannel);
-				timeout -> terminate
-			end;
+		{chunk, _, _, 0} ->
+			timeout_send; % give up on the chunk, and terminate the sender
+		{chunk, _, ChunkSequence, _} when ChunkSequence =< AckSequence ->
+			sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, AckSequence);
+		{chunk, Chunk, ChunkSequence, Retry} when ChunkSequence < AckSequence + 10 -> % window size
+			RPC_Cast(Name, {packet, Stream, Pid, ChunkSequence, Chunk, self()}),
+			timer:send_after(2000, {chunk, Chunk, ChunkSequence, Retry-1}), % schedule a retry
+			sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, AckSequence);
+		
+		{ack, Stream, Seq} when Seq > AckSequence ->
+			sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, Seq);
+		{ack, _, _} ->
+			sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, AckSequence); % ignore out-of-sequence acks
+			
+		{data, Data, _SendingPid} ->
+			NewSequence = chunkify(Data, Sequence),
+			sender_loop(RPC_Cast, Name, Stream, Pid, NewSequence, AckSequence);
+		
+		close when AckSequence >= Sequence - 1 ->
+			RPC_Cast(Name, {close, Stream, Pid, Sequence});
 		{'EXIT', _, _} ->
-			close(Server, Channel)
-	after 10000 ->
-		heartbeat(Server, Channel),
-		sender_loop(Server, Channel)
+			RPC_Cast(Name, {close, Stream, Pid, Sequence});
+		
+		heartbeat ->
+			RPC_Cast(Name, {heartbeat, Stream, Pid}),
+			timer:send_after(30000, heartbeat),
+			sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, AckSequence)
 	end.
 
+% Break a big binary into smaller chunks for sending.
+send_chunk(Chunk, Rest, Sequence) ->
+	self() ! {chunk, Chunk, Sequence, 60},
+	chunkify(Rest, Sequence + 1).
+chunkify(<<>>, Sequence) -> Sequence;
+chunkify(<<Chunk:4/binary, Rest/binary>>, Sequence) ->
+	send_chunk(Chunk, Rest, Sequence);
+chunkify(Chunk, Sequence) ->
+	send_chunk(Chunk, <<>>, Sequence).
+
+
+% Turn a channel that was send from remotely into a function that can be used to send data to that channel.
 make_sender(Server, Channel) ->
-	spawn_link(fun() ->
-		process_flag(trap_exit, true),
-		sender_loop(Server, Channel)
-	end).
+	gen_server:call(Server, {make_sender, Channel}).
 
-% Send a periodic heartbeat message to keep the session alive.
-heartbeat(Server, Channel) ->
-	gen_server:cast(Server, {heartbeat, Channel}).
-
-% Close a channel's write end.
-close(Server, Channel) ->
-	gen_server:call(Server, {close, Channel}).
-
-% Synchronously send data. Try to send bigger chunks at once. The bigger the better. They will be chunked if need be.
-% returns {ok, NewChannel} or timeout
-send(Server, Channel, Data) ->
-	gen_server:call(Server, {send, Channel, Data}, 30000).
-
-send_chunk(_, _, _, _, _, _, 0) -> timeout;
-send_chunk(RPC_Cast, Name, Stream, Pid, Sequence, Chunk, Retry) ->
-	RPC_Cast(Name, {packet, Stream, Pid, Sequence, Chunk, self()}),
-	receive
-		{ack, Stream, Sequence} -> ok
-	after 1000 ->
-		send_chunk(RPC_Cast, Name, Stream, Pid, Sequence, Chunk, Retry-1)
-	end.
-
-% returns {ok, NewChannel} or timeout
-send_chunks(_RPC_Cast, Name, Stream, Pid, Sequence, <<>>) -> {ok, {Name, Stream, Pid, Sequence}};
-send_chunks(RPC_Cast, Name, Stream, Pid, Sequence, <<Chunk:4096/binary, Rest/binary>>) ->
-	send_chunks(RPC_Cast, Name, Stream, Pid, Sequence, Chunk, Rest);
-send_chunks(RPC_Cast, Name, Stream, Pid, Sequence, Chunk) ->
-	send_chunks(RPC_Cast, Name, Stream,Pid, Sequence, Chunk, <<>>).
-send_chunks(RPC_Cast, Name, Stream, Pid, Sequence, Chunk, Rest) ->
-	case send_chunk(RPC_Cast, Name, Stream, Pid, Sequence, Chunk, 5) of
-			ok ->
-				send_chunks(RPC_Cast, Name, Stream, Pid, Sequence+1, Rest);
-			timeout ->
-				timeout
-	end.
-
-% Returns Channel.
-make_stream(Server) ->
+% Returns a new Channel.
+make_channel(Server) ->
 	gen_server:call(Server, {make_stream, self()}).
+	
+ready_to_receive({channel, _, _, StreamPid, _}) ->
+	StreamPid ! active.
 
 handle_call(Request, _Client, State) ->
 	{RPC_Cast, MyName} = State,
 	case Request of
 		{make_stream, Pid} ->
 			Stream = guid:new(),
-			InitSequence = 0,
-			StreamPid = spawn_link(fun() -> stream_worker(RPC_Cast, Stream, Pid, InitSequence) end),
-			{reply, {Stream, {MyName, Stream, StreamPid, InitSequence}}, State};
-			
-		{send, Channel, Data} ->
-			{Name, Stream, Pid, Sequence} = Channel,
-			Result = send_chunks(RPC_Cast, Name, Stream, Pid, Sequence, Data),
-			{reply, Result, State};
+			InitSequence = 1,
+			StreamPid = spawn_link(fun() ->
+				receiver_loop(RPC_Cast, {channel, MyName, Stream, self(), InitSequence}, InitSequence, Pid, inactive)
+			end),
+			Channel = {channel, MyName, Stream, StreamPid, InitSequence},
+			{reply, Channel, State};
 		
-		{close, Channel} ->
-			{Name, Stream, Pid, Sequence} = Channel,
-			RPC_Cast(Name, {close, Stream, Pid, Sequence}),
-			{reply, ok, State}
+		{make_sender, {channel, Name, Stream, Pid, Sequence}} ->
+			SenderPid = spawn_link(fun() ->
+				process_flag(trap_exit, true),
+				timer:send_after(5000, heartbeat),
+				sender_loop(RPC_Cast, Name, Stream, Pid, Sequence, Sequence - 1)
+			end),
+			Sender = fun
+				(close) -> SenderPid ! close;
+				(Data) when is_binary(Data) ->
+					SenderPid ! {data, Data, self()}
+			end,
+			
+			{reply, Sender, State}
 	end.
 	
-handle_cast(Request, State) ->
-	{RPC_Cast, _MyName} = State,
-	case Request of
-		{heartbeat, Channel} ->
-			{Name, Stream, Pid, _Sequence} = Channel,
-			RPC_Cast(Name, {heartbeat, Stream, Pid}),
-			{noreply, State}
-	end.
+handle_cast(_Request, State) -> {noreply, State}.
 
 handle_info(_Request, State) -> {noreply, State}.
 	
